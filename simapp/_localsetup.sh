08efabf68ae1f9015aeb9936aea3c11ba7f5a7e1
antstalepresh <stalepresh121@outlook.com>
2020-10-15 02:21:15 +1000

    update test commands for localsetup

--- simapp/_localsetup.sh
+++ simapp/_localsetup.sh
@@ -12,29 +12,13 @@ simd collect-gentxs --home=$HOME/.simd
 
 simd start --home=$HOME/.simd
 
-# simd tx bank send validator cosmos1gyq9gpp5geh0a0qhyvckjexc848rkp3dgtfg82 1000stake  --keyring-backend=test --home=$HOME/.simd --chain-id=testing
+# simd tx bank send validator cosmos1gyq9gpp5geh0a0qhyvckjexc848rkp3dgtfg82 1000stake  --keyring-backend=test --home=$HOME/.simd --chain-id=testing <<< y
 # simd keys show -a validator --keyring-backend=test --home=$HOME/.simd
-# cosmos1flthcq9e0jv957q60y649ll8jq3j2gz0w3fk7n
 # cosmos1gyq9gpp5geh0a0qhyvckjexc848rkp3dgtfg82
 
-# simd keys show test1_new  --keyring-backend=test
-# - name: test1_new
-#   type: local
-#   address: cosmos1qrg2mj7p44kd4uvdquq7mk8x8hs4nsetn6dc4m
-#   pubkey: cosmospub1addwnpepqv9zyn03cdjsylsdg26m8x3tpmkccsa6989948n6nwk84qs407fpvs2lk95
-#   mnemonic: ""
-#   threshold: 0
-#   pubkeys: []
-# simd keys change-address test1_new cosmos16cnu2pxhg66zau3rua8nvuha7jm7glrq0wtpkc --keyring-backend=test
-# Key address will be updated. Continue? [y/N]: y
-# simd keys show test1_new  --keyring-backend=test
-# - name: test1_new
-#   type: local
-#   address: cosmos16cnu2pxhg66zau3rua8nvuha7jm7glrq0wtpkc
-#   pubkey: cosmospub1addwnpepqv9zyn03cdjsylsdg26m8x3tpmkccsa6989948n6nwk84qs407fpvs2lk95
-#   mnemonic: ""
-#   threshold: 0
-#   pubkeys: [] 
+# simd keys show validator  --keyring-backend=test --home=$HOME/.simd
+# simd keys change-address validator cosmos1gyq9gpp5geh0a0qhyvckjexc848rkp3dgtfg82 --keyring-backend=test --home=$HOME/.simd  <<< y
+# simd keys show validator  --keyring-backend=test --home=$HOME/.simd
 
 # simd tx bank send validator cosmos1gyq9gpp5geh0a0qhyvckjexc848rkp3dgtfg82 1000stake  --keyring-backend=test --home=$HOME/.simd --chain-id=testing
 # {
